dc6c062a914546b364dd2ff2f40ea24123c647f3
joytrinh <mongtuyen149@gmail.com>
2019-04-20 23:04:30 +0700

    Tao Phien ban Xem_Quan Ly

---
+++ QuanLy/QuanLy/XuLy/3A-Mo_hinh_Doi_tuong.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class XL_KhuVuc
+{
+    public string MaSo, Ten;
+    public int SoTang;
+}
+public class XL_TiepTan
+{
+    public string MaSo, HoTen, TenDangNhap, MatKhau;
+    public XL_KhuVuc KhuVuc = new XL_KhuVuc();
+}
+public class XL_QuanLy
+{
+    public string MaSo, HoTen, TenDangNhap, MatKhau;
+    public List<XL_KhuVuc> DanhSachKhuVuc = new List<XL_KhuVuc>();
+}
+public class XL_Phong
+{
+    public string MaSo, Ten;
+    public int TrangThai;
+    public XL_LoaiPhong LoaiPhong = new XL_LoaiPhong();
+    public XL_KhuVuc KhuVuc = new XL_KhuVuc();
+}
+public class XL_LoaiPhong
+{
+    public string MaSo, Ten, TienNghi;
+    public int DonGia, SoKhachToiDa;
+}
+public class XL_PhieuThuePhong
+{
+    public string MaSo, TenPhong;
+    public DateTime NgayBatDau, NgayDuKienTra, NgayTraPhong;
+    public List<XL_KhachHang> DanhSachKhachHang = new List<XL_KhachHang>();
+}
+public class XL_KhachHang
+{
+    public string HoTen, CMND;
+}
+public class XL_KhachSan
+{
+    public string MaSo, Ten, DienThoai, DiaChi;
+    public List<XL_KhuVuc> DanhSachKhuVuc = new List<XL_KhuVuc>();
+    public List<XL_LoaiPhong> DanhSachLoaiPhong = new List<XL_LoaiPhong>();
+}
